eb0de1de2dbfcb04f371e1a256a4f289c1e3015a
DeadLeg1on <shuharenkostas@gmail.com>
2019-10-12 15:01:28 +0300

    Создание репозитория

---
+++ Assets/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject hook;
+
+    Rigidbody2D rb;
+    GameObject player;
+    Vector2 hookPos;
+    Vector2 playerPos;
+
+    int speed = 50;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        PlayerControl();
+    }
+    
+    private void PlayerControl()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            HookFire();
+        }
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb.AddForce(movement * speed) ;
+    }
+    
+    private void HookFire()
+    {   
+           // int hookSpeed = 5;
+            hook = Instantiate(hook); //Создаем
+            playerPos = player.transform.position; // координаты игрока
+            hook.transform.position = playerPos; //Начальные кординаты хука
+            hookPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Куда лететь
+            hook.GetComponent<TargetJoint2D>().target = hookPos;
+           // hook.target = hookPos;
+    }
+    
+}
